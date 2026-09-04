7a9b8ca0409d502b862bcb346a7da15b59fbda0d
Yassine Boubkeur <boubkeuryassine2@gmail.com>
2026-09-04 20:44:54 +0100

    commit DevOps & Deployment

---
+++ docker/postgres/init.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE shopsphere_auth;
+CREATE DATABASE shopsphere_product;
+CREATE DATABASE shopsphere_order;
+CREATE DATABASE shopsphere_payment;
+CREATE DATABASE shopsphere_inventory;
+CREATE DATABASE shopsphere_notification;
+CREATE DATABASE shopsphere_analytics;
